1044a22dc385ca82c5bb1dd83e1dabb2e64c2bee
patra dinata <patradinata08@gmail.com>
2025-10-23 10:01:22 +0700

    add loading animation

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,21 +2,29 @@ import React, { useState } from "react";
 import "./Login.css";
 import logo from "../../assets/logo.png";
 import { login, signUp } from "../../firebase";
+import netflix_spinner from "../../assets/netflix_spinner.gif";
 
 const Login = () => {
   const [signState, setSignState] = useState("Sign In");
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
   const userAuth = async (event) => {
     event.preventDefault();
+    setLoading(true);
     if (signState === "Sign In") {
       await login(email, password);
     } else {
       await signUp(name, email, password);
     }
+    setLoading(false);
   };
-  return (
+  return loading ? (
+    <div className="login-spinner">
+      <img src={netflix_spinner} alt="" />
+    </div>
+  ) : (
     <div className="login">
       <img src={logo} className="login-logo" alt="" />
       <div className="login-form">
